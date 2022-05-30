63de8f7c218b1af562a01fe83a99799af0c57fa9
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-05-30 03:35:40 -0300

    a

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,50 @@
 // @ts-nocheck
 import { PrismaClient } from "@prisma/client";
+async function main() {
+   for (let investment = 0; investment < 30; investment++) {
+      await prisma.investment.create({
+         data: {
+            name: `${faker.name.firstName()}`,
+            nameComplete: `${faker.name.firstName()} ${faker.name.lastName()}`,
+            descricacao: `${faker.name.firstName()} é uma empresa de tal para tal ramo`,
+            captado: 2,
+            reservado: 0,
+            objetivo: 5,
+            alvo_minimo: 250000,
+            alvo_maximo: 500000,
+            valor_minimo: 20,
+            rentabilidade_alvo: "0.2% ao ano",
+            pagamentos_projetados: "Ao final",
+            modalidade: "Equity",
+            participacao: 2,
+            oportunidade: "Expansão de operação",
+            setor: "Empresas multinacionais",
+            createdAt: faker.date.recent(35)
+         }
+      });
+   }
+
+   // Create admin user
+
+   await prisma.user.create({
+      data: {
+         name: "Administrador",
+         email: "administrador@bridge.net",
+         password: await bcrypt.hash("admin", 8),
+         createdAt: new Date()
+      }
+   });
+}
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
    prisma = new PrismaClient();
+   main()
+      .catch(() => process.exit(1))
+      .finally(() => {
+         prisma.$disconnect();
+      });
 } else {
    if (!global.prisma) {
       global.prisma = new PrismaClient();
